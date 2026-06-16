b46566976561fa865ec77ccfd58c2d8587b23f38
ing-melcat <ing-melcat@users.noreply.github.com>
2026-06-16 17:39:38 -0600

    fix: recortes de imagenes de intro corregidos y creados con exito

--- crop.ps1
+++ crop.ps1
@@ -1,8 +1,8 @@
 Add-Type -AssemblyName System.Drawing
 $srcPath = "C:\Users\USER\Desktop\CitaCiudadana_Completo\images\sprite_intro2.png"
 $srcImg = [System.Drawing.Image]::FromFile($srcPath)
-$w = [Math]::Floor($srcImg.Width / 3)
-$h = $srcImg.Height
+$w = [int][Math]::Floor($srcImg.Width / 3)
+$h = [int]$srcImg.Height
 
 $rect3 = New-Object System.Drawing.Rectangle(0, 0, $w, $h)
 $dest3 = New-Object System.Drawing.Bitmap($w, $h)
